720a3096d80145d27764cd21aa548a0e8ca05553
freyja <mrender2005@gmail.com>
2021-08-28 16:22:50 -0700

    flet, lables

--- learn.lisp
+++ learn.lisp
@@ -2,4 +2,15 @@
 ;; lisp-learning notebook
 
 (let ((a 5) (b 6))
-	(print (+ a b)))
+	(+ a b))
+
+(flet ((square (n)
+		(* n n)))
+	(defvar five-squared (square 5))
+	(print five-squared))
+
+(labels ((square (n)
+		(* n n))
+	 (area (r)
+	 	(* pi (square r))))
+	(print (area 5)))
